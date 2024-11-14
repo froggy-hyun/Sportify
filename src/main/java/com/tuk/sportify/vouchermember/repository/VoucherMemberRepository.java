56d99195d1cc451fc3e006f64b5b2072e494bb6f
songhaechan <bukak2019@naver.com>
2024-11-14 17:45:33 +0900

    fix: JPQL 쿼리 변경

--- src/main/java/com/tuk/sportify/vouchermember/repository/VoucherMemberRepository.java
+++ src/main/java/com/tuk/sportify/vouchermember/repository/VoucherMemberRepository.java
@@ -3,8 +3,7 @@ package com.tuk.sportify.vouchermember.repository;
 import com.tuk.sportify.member.domain.Member;
 import com.tuk.sportify.sportvoucher.domain.SportVoucher;
 import com.tuk.sportify.vouchermember.domain.VoucherMember;
-import java.util.List;
-import java.util.Optional;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,11 +14,11 @@ import org.springframework.stereotype.Repository;
 public interface VoucherMemberRepository extends JpaRepository<VoucherMember, Long> {
 
     // 현재 사용중인 개인 이용권 조회
-    @Query("select vm.sportVoucher from VoucherMember vm join fetch vm.sportVoucher where vm.member =: member")
+    @Query("select vm from VoucherMember vm join fetch vm.sportVoucher where vm.member =: member")
     Slice<SportVoucher> findSportVoucherByMemberJoinFetch(Member member, PageRequest pageRequest);
 
     // 현재 참여중인 크루에 대한 이용권 조회
     @Query("select vm from VoucherMember vm join fetch vm.sportVoucher"
-        + " join fetch vm.crew where vm.member =: member limit =:limit")
+        + " join fetch vm.crew where vm.member =: member")
     Slice<VoucherMember> findByMemberJoinFetch(Member member, PageRequest pageRequest);
 }
