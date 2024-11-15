df92fff9639aca429d066b704cf4fea06a90cdf9
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-11-15 16:51:09 +0900

    Update issue templates

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import HomePage from './pages/HomePage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </BrowserRouter>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+          </Routes>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </BrowserRouter>
+      </RecoilRoot>
     </QueryClientProvider>
   );
 }
