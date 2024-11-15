27540273b70c3ecbee77d958d050ee9b432f4322
songhaechan <bukak2019@naver.com>
2024-11-15 16:09:03 +0900

    fix: VoucherMemberRepository JPQL 수정

--- src/main/java/com/tuk/sportify/vouchermember/repository/VoucherMemberRepository.java
+++ src/main/java/com/tuk/sportify/vouchermember/repository/VoucherMemberRepository.java
@@ -4,19 +4,19 @@ import com.tuk.sportify.member.domain.Member;
 import com.tuk.sportify.sportvoucher.domain.SportVoucher;
 import com.tuk.sportify.vouchermember.domain.VoucherMember;
 
-import java.util.List;
 import org.springframework.data.domain.Limit;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VoucherMemberRepository extends JpaRepository<VoucherMember, Long> {
 
     // 현재 사용중인 개인 이용권 조회
-    @Query("select vm from VoucherMember vm join fetch vm.sportVoucher where vm.member =: member")
+    @Query("select vm from VoucherMember vm join fetch vm.sportVoucher where vm.member =: member "
+        + "and vm.crew =:null")
     List<SportVoucher> findSportVoucherByMemberJoinFetch(Member member, Limit limit);
 
     // 현재 참여중인 크루에 대한 이용권 조회
