9430dd14e7f8fd1d82cd84bc39aa385b9221bb8f
froggy-hyun <1233day@naver.com>
2024-11-18 01:29:24 +0900

    fix: ErrorCode 병합

--- src/main/java/com/tuk/sportify/global/error/ErrorCode.java
+++ src/main/java/com/tuk/sportify/global/error/ErrorCode.java
@@ -16,7 +16,21 @@ public enum ErrorCode {
     /**
      * Crew : 크루 관련 에러 코드
      */
-    CREW_NOT_FOUND(HttpStatus.BAD_REQUEST,"201","존재하지 않는 크루입니다.");
+    CREW_NOT_FOUND(HttpStatus.BAD_REQUEST,"201","존재하지 않는 크루입니다."),
+
+    /**
+     * Member : 멤버 관련 에러 코드
+     */
+    MEMBER_EMAIL_NOT_VERIFIED(HttpStatus.BAD_REQUEST, "410", "Email not verified"),
+    WRONG_PARAMETER(HttpStatus.BAD_REQUEST, "420", "Wrong parameter"),
+    LOGIN_FAILED(HttpStatus.BAD_REQUEST, "430", "Login failed"),
+    SERVER_ERROR(HttpStatus.BAD_REQUEST, "500", "Server error"),
+    TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "4011", "Token expired"),
+    TOKEN_IS_BLACKLIST(HttpStatus.BAD_REQUEST, "4012", "Token is blacklisted"),
+    TOKEN_WRONG_SIGNATURE(HttpStatus.BAD_REQUEST, "4013", "Token has wrong signature"),
+    TOKEN_HASH_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "4014", "Token hash algorithm not supported"),
+    NO_AUTH_HEADER(HttpStatus.BAD_REQUEST, "4015", "Authorization header is missing"),
+    TOKEN_VALIDATION_TRY_FAILED(HttpStatus.BAD_REQUEST, "4016", "Token validation attempt failed");
 
     private final HttpStatus httpStatus;
     private final String code;
