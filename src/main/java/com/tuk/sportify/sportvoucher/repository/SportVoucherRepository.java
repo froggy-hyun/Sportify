de5b5c04cfb1c57d4e4da3b0b961bed878ea1651
songhaechan <bukak2019@naver.com>
2024-11-17 23:05:24 +0900

    refactor: 인기이용권 쿼리 수정

--- src/main/java/com/tuk/sportify/sportvoucher/repository/SportVoucherRepository.java
+++ src/main/java/com/tuk/sportify/sportvoucher/repository/SportVoucherRepository.java
@@ -1,11 +1,13 @@
 package com.tuk.sportify.sportvoucher.repository;
 
 import com.tuk.sportify.sportvoucher.domain.SportVoucher;
-import java.util.List;
+
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SportVoucherRepository extends JpaRepository<SportVoucher, Long> {
@@ -15,12 +17,4 @@ public interface SportVoucherRepository extends JpaRepository<SportVoucher, Long
                     + " and sv.course.endAt > :currentDate order by sv.popularity desc")
     List<SportVoucher> findPopularVoucherByCityAndGu(
             String city, String gu, Integer currentDate, Limit limit);
-
-    @Query(
-            "select sv from SportVoucher sv where sv.facility.city =:city and sv.facility.gu =:gu "
-                    + " and sv.course.endAt > :currentDate ")
-    List<SportVoucher> findNewVoucherByCityAndGu(
-            String city, String gu, Integer currentDate, Limit limit);
-
-
 }
