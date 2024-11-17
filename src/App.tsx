ba67bb6fb49a859e5cf444f620eed633c04d7dd9
76Hee <105695940+76Hee@users.noreply.github.com>
2024-11-18 04:51:39 +0900

    feat: 티켓 신청 페이지 추가 및 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import TicketApplyPage from './pages/TicketApplyPage';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
@@ -13,6 +15,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<HomePage />}></Route>
+            <Route path="/ticket" element={<TicketApplyPage />}></Route>
           </Routes>
           <ReactQueryDevtools initialIsOpen={false} />
         </BrowserRouter>
---
+++ src/pages/TicketApplyPage.tsx
@@ -0,0 +1,10 @@
+const TicketApplyPage = () => {
+
+    return (
+        <div>
+            <h1>티켓 신청</h1>
+        </div>
+    );
+  };
+  
+  export default TicketApplyPage;
