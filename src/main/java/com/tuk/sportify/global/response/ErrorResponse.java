9e0ba35b01077f8a3701022cead2c9b2c5bc5a1b
songhaechan <bukak2019@naver.com>
2024-11-17 22:05:43 +0900

    feat: HttpStatusCode를 에러 응답에 추가

--- src/main/java/com/tuk/sportify/global/response/ErrorResponse.java
+++ src/main/java/com/tuk/sportify/global/response/ErrorResponse.java
@@ -2,11 +2,11 @@ package com.tuk.sportify.global.response;
 
 import com.tuk.sportify.global.error.ErrorCode;
 
-public record ErrorResponse(String status, String code, String message) {
+public record ErrorResponse(String status, int httpStatusCode, String code, String message) {
 
     private static final String FAILURE = "failure";
 
     public ErrorResponse(final ErrorCode errorCode) {
-        this(FAILURE, errorCode.getCode(), errorCode.getMsg());
+        this(FAILURE, errorCode.getHttpStatus().value(), errorCode.getCode(), errorCode.getMsg());
     }
 }
