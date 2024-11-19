d81d80b7672f0801e18dbb1bf09473df6a22fd9d
songhaechan <bukak2019@naver.com>
2024-11-19 16:33:58 +0900

    chore: ErrorResponse 수정

--- src/main/java/com/tuk/sportify/global/response/ErrorResponse.java
+++ src/main/java/com/tuk/sportify/global/response/ErrorResponse.java
@@ -3,7 +3,7 @@ package com.tuk.sportify.global.response;
 import com.tuk.sportify.global.status_code.ErrorCode;
 
 public record ErrorResponse(
-        int httpStatusCode, String httpStatusMessage, String code, String serverMessage) {
+        int httpStatusCode, String httpStatusMessage, String serverCode, String serverMessage) {
 
     public ErrorResponse(final ErrorCode errorCode) {
         this(
