967356b104f7892343218763280d05a36c43cedb
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-11-19 23:57:23 +0900

    Header 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import { Header } from './components';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
+        <Header />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<HomePage />}></Route>
---
+++ src/styles/componentsSyles/Header.styled.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+
+
+
+export const HeaderContainer = styled.div`
+position: fixed;
+top:0;
+width: 100%;
+height: 6.4rem;
+background-color:  var(--white);
+display: flex;
+justify-content: space-between;
+padding: 2rem;
+
+`;
+
+
+export const NoticeAndSearch = styled.img`
+   height: 2.4rem;
+   width: 2.4rem;
+   &.search {
+    margin-left: 2rem;
+
+  }
+`;
+
+
+export const LogoImg= styled.img`
+  width: 10.7rem;
+  height: 2.4rem;
+`;
+
+
+
