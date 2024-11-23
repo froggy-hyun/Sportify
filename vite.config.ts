04a169ca3bfc3b04abc481bfbfa2f45fddee618a
Kim HeeChan <h22chan.dev@gmail.com>
2024-11-23 16:27:21 +0900

    fix : test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
 import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
 export default defineConfig({
-  plugins: [react(),VitePluginHtmlEnv(),VitePluginHtmlEnv({
+  plugins: [react(), tsconfigPaths(), VitePluginHtmlEnv(),VitePluginHtmlEnv({
       compiler: true
     })
   ]
