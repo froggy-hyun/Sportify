8ab093574641a81dfa99fb2b2f34d6100f0d3b93
songhaechan <bukak2019@naver.com>
2024-11-27 17:12:48 +0900

    docs: Docs Example 추가

--- src/main/java/com/tuk/sportify/crew/dto/CreateCrewRequest.java
+++ src/main/java/com/tuk/sportify/crew/dto/CreateCrewRequest.java
@@ -13,10 +13,16 @@ import java.util.List;
 @Schema
 public record CreateCrewRequest(
         @Schema(description = "크루 이름") String crewName,
-        @Size(max = 3, message = "목표는 3개까지 지정 가능합니다.") @Schema(description = "목표") List<Goal> goals,
-        @Schema(description = "성별 제한") GenderRule genderRule,
-        @Size(max = 3, message = "규칙은 3개까지 지정 가능합니다.") @Schema(description = "규칙")
+        @Size(max = 3, message = "목표는 3개까지 지정 가능합니다.")
+                @Schema(
+                        description = "목표",
+                        example =
+                                "[\"FITNESS_ENHANCEMENT\", \"HEALTH_MAINTENANCE\", \"ACHIEVEMENT\"]")
+                List<Goal> goals,
+        @Schema(description = "성별 제한", example = "DONT_CARE") GenderRule genderRule,
+        @Size(max = 3, message = "규칙은 3개까지 지정 가능합니다.")
+                @Schema(description = "규칙", example = "[\"지각하면 벌금\", \"지각 안해도 벌금\", \"그냥 벌금\"]")
                 List<String> rules,
-        @Schema(description = "난이도") DifficultyLevel difficultyLevel,
-        @Schema(description = "수용 인원") Integer capacity,
+        @Schema(description = "난이도", example = "INTERMEDIATE") DifficultyLevel difficultyLevel,
+        @Schema(description = "수용 인원", example = "5") Integer capacity,
         @Schema(description = "이미지 ID") Long imageId) {}
