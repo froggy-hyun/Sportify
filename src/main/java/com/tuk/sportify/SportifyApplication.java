12d433226bc759aec332a08e2062c5e52f5d0c30
songhaechan <bukak2019@naver.com>
2024-11-27 09:56:22 +0900

    hotfix: Swagger 요청 상대경로로 변경

--- src/main/java/com/tuk/sportify/SportifyApplication.java
+++ src/main/java/com/tuk/sportify/SportifyApplication.java
@@ -1,9 +1,13 @@
 package com.tuk.sportify;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = @Server(url="/",description = "default url"))
 public class SportifyApplication {
 
     public static void main(String[] args) {
