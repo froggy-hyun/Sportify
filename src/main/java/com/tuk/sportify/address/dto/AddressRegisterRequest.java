12c676043f12466de86f57ddf06aef4effd30328
songhaechan <bukak2019@naver.com>
2024-11-27 16:27:18 +0900

    setting: DockerCompose 수정, Workflow 수정

--- src/main/java/com/tuk/sportify/address/dto/AddressRegisterRequest.java
+++ src/main/java/com/tuk/sportify/address/dto/AddressRegisterRequest.java
@@ -4,12 +4,12 @@ package com.tuk.sportify.address.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record AddressRegisterRequest(
-    @Schema(description = "주소지 위도")
+    @Schema(description = "주소지 위도", example = "37.57015")
     double latitude,
-    @Schema(description = "주소지 경도")
+    @Schema(description = "주소지 경도", example = "126.9772")
     double longitude,
-    @Schema(description = "주소지")
+    @Schema(description = "주소지",example = "서울특별시 종로구 세종로 139-1")
     String address,
-    @Schema(description = "주소지 이름")
+    @Schema(description = "주소지 이름", example = "회사")
     String addressName
 ) {}
