8ce85745f5ddb0e2cbf785521f942a5f80b84099
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-12-04 01:02:28 +0900

    fix : 현재 설정 주소 세션으로 관리

--- src/components/ui/Location.tsx
+++ src/components/ui/Location.tsx
@@ -1,16 +1,16 @@
-import { useRecoilValue } from 'recoil';
-import { userAddressState } from '../../recoil/atom/userLocation';
-
 import * as S from '@/styles/pagesStyles/homeStyles/Location.styled';
 import arrowImg from '@/assets/icon/etc/arrow/rightArrow_White.png';
 import { useNavigate } from 'react-router-dom';
 
 const Location = () => {
-  const userAddress = useRecoilValue(userAddressState);
+  const location = sessionStorage.getItem('currentLocation');
+
   const navigate = useNavigate();
   return (
     <S.LocationContainer>
-      <S.LocationTitle>{userAddress}</S.LocationTitle>
+      <S.LocationTitle>
+        {!location ? '서울 중구 세종대로 110 서울특별시청' : location}
+      </S.LocationTitle>
       <S.LocationImg src={arrowImg} alt="우측 화살표" onClick={() => navigate('/addressSearch')} />
     </S.LocationContainer>
   );
