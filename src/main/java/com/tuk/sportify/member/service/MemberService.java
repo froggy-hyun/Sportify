51180be1771cde5a551020890b5d81f458c6331e
froggy-hyun <1233day@naver.com>
2024-12-05 11:04:58 +0900

    fix: 비밀번호 조건 최소 8자리만 유지하도록 변경

--- src/main/java/com/tuk/sportify/member/service/MemberService.java
+++ src/main/java/com/tuk/sportify/member/service/MemberService.java
@@ -34,7 +34,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    private static final String PASSWORD_REGEX = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$"; // 최소 8자리 + 영어, 숫자, 특수문자를 모두 포함해야함.
+    private static final String PASSWORD_REGEX = "^.{8,}$"; // 문자 종류 상관 없이 최소 8자리
     private static final Pattern PASSWORD_PATTERN = Pattern.compile(PASSWORD_REGEX);
 
     private final MemberMapper memberMapper;
