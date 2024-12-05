e41a2f898f7d4634b00cf81170f111f8e9d7ebb9
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-12-05 13:36:00 +0900

    fix :  pwa 에러 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
@@ -8,3 +9,8 @@ createRoot(document.getElementById('root')!).render(
 
   <App />
 );
+// 서비스 워커 등록
+
+if ('serviceWorker' in navigator) {
+  registerSW(); // 서비스 워커 등록
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,52 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import VitePluginHtmlEnv from 'vite-plugin-html-env';
-
+import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
     VitePluginHtmlEnv({
       compiler: true,
-    }),
+    }),VitePWA({
+      registerType: 'autoUpdate',
+      devOptions:{enabled: true, type: 'module'}, // vite dev 로 돌려도 PWA 까지 볼 수 있게끔 주는 옵션
+      includeAssets: [
+      '/icon/launchericon-48-48.png',
+      '/icon/launchericon-96-96.png',
+      '/icon/launchericon-192-192.png',
+      '/icon/launchericon-512-512.png',
+      ], 
+      manifest: {
+          name: 'sportify',
+          short_name: 'sportify',
+          start_url: "/",
+          background_color: "#0096C7",
+          display: "fullscreen",
+          theme_color: '#0096C7',
+          icons:[
+            {
+              src:  '/icon/launchericon-48-48.png',
+              sizes: '48x48',
+              type: 'image/png',
+            },
+            {
+              src:  '/icon/launchericon-96-96.png',
+              sizes: '96x96',
+              type: 'image/png',
+            },
+            {
+              src:  '/icon/launchericon-192-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src:  '/icon/launchericon-512-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ]
+        }
+      })
   ],
 });
