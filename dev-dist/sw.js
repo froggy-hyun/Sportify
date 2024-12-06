39fdbea9864def53937484d965dc5813aefbff3f
Kim HeeChan <h22chan.dev@gmail.com>
2024-12-06 20:54:42 +0900

    fix : PWA 프리캐싱

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1u45lltpubo"
+    "revision": "0.vs2chrae5po"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
