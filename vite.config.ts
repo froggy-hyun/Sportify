62bbd5f90b057be2b47f846cd1cea11898980a86
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-12-07 02:06:02 +0900

    feat : 180 X 180 아이콘 추가

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
           name: 'sportify',
           short_name: 'sportify',
           start_url: "/",
-          background_color: "#0096C7",
+          background_color: "#000000",
           display: "fullscreen",
           theme_color: '#0096C7',
           icons:[
@@ -38,21 +38,31 @@ export default defineConfig({
               src:  '/icon/launchericon-48-48.png',
               sizes: '48x48',
               type: 'image/png',
+               purpose: "any maskable"
             },
             {
               src:  '/icon/launchericon-96-96.png',
               sizes: '96x96',
               type: 'image/png',
+               purpose: "any maskable"
+            },
+            {
+              src:  '/icon/launchericon-180-180.png',
+              sizes: '180x180',
+              type: 'image/png',
+              purpose: "any maskable"
             },
             {
               src:  '/icon/launchericon-192-192.png',
               sizes: '192x192',
               type: 'image/png',
+               purpose: "any maskable"
             },
             {
               src:  '/icon/launchericon-512-512.png',
               sizes: '512x512',
               type: 'image/png',
+               purpose: "any maskable"
             },
           ]
         }
