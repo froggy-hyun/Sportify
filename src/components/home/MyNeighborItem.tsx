70d78f2a3209c9c3b4241ae023c3715ec0d4af25
Kim Hee Chan <a01049984394k@gmail.com>
2024-12-07 23:25:36 +0900

    fix : 메인페이지 나의 이웃 아이콘 활성화

--- src/components/home/MyNeighborItem.tsx
+++ src/components/home/MyNeighborItem.tsx
@@ -1,11 +1,11 @@
 import * as S from '@/styles/pagesStyles/homeStyles/MyNeighborItem.styled';
-// import arrowImg from '@/assets/icon/etc/arrow/rightArrow_Default.png';
+import arrowImg from '@/assets/icon/etc/arrow/rightArrow_Default.png';
 
 const MyNeighborItem = ({ title }: { title: string }) => {
   return (
     <S.MyNeighborContainer>
       <S.MyNeighborTitle>{title}</S.MyNeighborTitle>
-      {/* <S.ArrowImg src={arrowImg} alt="바로가기" /> */}
+      <S.ArrowImg src={arrowImg} alt="바로가기" />
     </S.MyNeighborContainer>
   );
 };
